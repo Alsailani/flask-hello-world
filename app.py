1b97dabdd7b40c8d4241e841dc5235e65c3b4a48
Alsailani <moal5633@colorado.edu>
2024-11-02 17:09:42 +0000

    updated db_test

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from -- Mohammed Alsailani -- in 3308'
+
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://mohammed_db_user:UmSc7JQQWVM3IqL8sbwxtGBI8I4cINRV@dpg-csj5d6btq21c73d9b840-a/mohammed_db")
+    conn.close()
+    return "Database Connection Successful"
