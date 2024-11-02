c8f7dc181a2dc143834e6103f9c1abee9677d8e4
Alsailani <moal5633@colorado.edu>
2024-11-02 17:26:10 +0000

    Test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from -- Mohammed Alsailani -- in 3308'
+    return 'Hello World from -- Mohammed Alsailani -- in 3308 !'
 
 
 @app.route('/db_test')
